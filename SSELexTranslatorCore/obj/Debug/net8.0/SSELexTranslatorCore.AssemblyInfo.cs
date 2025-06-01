484ae977501fb52f53db20b5dc06b8d48abb4a46
YD525 <525084464@qq.com>
2025-06-01 16:36:32 +0800

    UPDate

--- SSELexTranslatorCore/obj/Debug/net8.0/SSELexTranslatorCore.AssemblyInfo.cs
+++ SSELexTranslatorCore/obj/Debug/net8.0/SSELexTranslatorCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SSELexTranslatorCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c8d112073e43197bbd30227cec8869d58e0b73f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SSELexTranslatorCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SSELexTranslatorCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
